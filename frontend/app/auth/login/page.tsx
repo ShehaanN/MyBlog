d0d3cdcbd3d34de707df5a20ae2b38e65e033984
ShehaanN <shehannadeesha2580@gmail.com>
2025-12-11 22:55:40 +0530

    feat: enhance login page with form

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,10 +1,72 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Submitted:", { email, password });
+  };
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <main className="min-h-screen bg-background flex flex-col">
+      {/* Login Form */}
+      <div className=" flex-1 flex items-center justify-center px-6 py-12">
+        <div className="bg-white p-6 rounded-lg w-full max-w-lg space-y-8">
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold">Welcome Back</h1>
+            <p className="text-muted-foreground">
+              Sign in to your MyBlog account
+            </p>
+          </div>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <Button type="submit" size="lg" className="w-full">
+              Sign In
+            </Button>
+          </form>
+          <div className="text-center text-sm">
+            <span className="text-muted-foreground">
+              Don&apos;t have an account?{" "}
+            </span>
+            <Link
+              href="/auth/signup"
+              className="text-primary hover:underline font-medium"
+            >
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
