281f9f777d54be12a481521717c959300c5e92cd
ShehaanN <shehannadeesha2580@gmail.com>
2025-12-11 11:54:29 +0530

    chore: initialize frontend with Next.js, TypeScript, and Tailwind CSS

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <p>Welcome to my blog page!</p>
+    </div>
+  );
+};
+
+export default page;
