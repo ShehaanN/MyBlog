36428b6884713039ae610865a3e0f65d84e63b2f
ShehaanN <shehannadeesha2580@gmail.com>
2025-12-14 15:26:35 +0530

    feat: add authentication module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module.js';
 import { CategoriesModule } from './categories/categories.module.js';
 import { PrismaModule } from './config/prisma/prisma.module.js';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PostsModule, CategoriesModule, PrismaModule],
+  imports: [PostsModule, CategoriesModule, PrismaModule, AuthModule],
 })
 export class AppModule {}
