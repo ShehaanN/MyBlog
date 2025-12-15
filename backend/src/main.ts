721817bc95cdda5ab6aa5f44d6c7de3f5939e3f4
ShehaanN <shehannadeesha2580@gmail.com>
2025-12-15 23:32:15 +0530

    fix:Fix CORS configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,10 @@ import { AppModule } from './app.module.js';
 async function main() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://myblog-vert.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://myblog-vert.vercel.app'],
     credentials: true,
   });
-  app.setGlobalPrefix('/api/v1');
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 4000);
 }
 main();
