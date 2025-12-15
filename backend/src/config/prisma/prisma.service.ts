9dd09e6b242ebfd7a3e0177373ecd50256d4b6ff
ShehaanN <shehannadeesha2580@gmail.com>
2025-12-15 19:06:52 +0530

    refactor: update PrismaService

--- backend/src/config/prisma/prisma.service.ts
+++ backend/src/config/prisma/prisma.service.ts
@@ -1,17 +1,13 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '../../../generated/prisma/client.js';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma/client.js';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor() {
-    const adapter = new PrismaMariaDb({
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      connectionLimit: 5,
-    });
-    super({ adapter });
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
